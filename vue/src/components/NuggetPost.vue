d4f75b08ff9cbd78bed9363b5b8faf2529abce38
Kavirthan KARUNAKARAN <kavirthan.karunakaran@isae-supaero.fr>
2022-08-01 14:28:40 +0200

    compilation

--- vue/src/components/NuggetPost.vue
+++ vue/src/components/NuggetPost.vue
@@ -2,7 +2,7 @@
   <div class="nugget-post">
     <img style="width:100%" :src="post.nugget_thumbnail_url.concat('?width=700&height=394')" alt="">
     <h3>{{ post.name }}</h3>
-    <h5>by {{ post.authors_name.join(", ") }}</h5>
+    <h5 v-if="post.authors_name">by {{ post.authors_name.join(", ") }}</h5>
     <p>{{ post.resume | truncate(190, "...") }}</p>
     <p>{{ post.displayinfo }}</p>
   </div>
