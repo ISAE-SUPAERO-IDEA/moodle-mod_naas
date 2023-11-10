7914851415a158b2dab493b03904a774605651f1
Kavirthan KARUNAKARAN <kavirthan.karunakaran@isae-supaero.fr>
2023-11-10 08:28:37 +0100

    change style

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version        = 2023110101;                   // The current module version (Date: YYYYMMDDXX)
+$plugin->version        = 2023110900;                   // The current module version (Date: YYYYMMDDXX)
 $plugin->requires       = 2019051100;                   // Requires this Moodle version
 $plugin->component      = 'mod_naas';                   // Full name of the plugin (used for diagnostics)
 $plugin->cron           = 0;                            // Frequency of the plugin's cron task
