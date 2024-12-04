e7246f19a5c0a843baffae784919d31ca19a3b4d
Sophie Bernard <133211066+sb-idea@users.noreply.github.com>
2024-12-04 17:46:09 +0100

    Chore on language translate file en (#9)
    * delete lang/fr according to translate in amos preferable
    
    * add comment in headers lang/en file: type of messages and modif copyright
    
    * add capabilities messages translate in en
    
    * Group messages by blacks, and blocks sorted alphabetically
    
    * change texte modulename_help with html tag
    
    * fix a potential malfunction with AMOS on multi-line, values set on a single line
    
    ---------
    
    Co-authored-by: sbernard <sbernard@PluginMoodle.myguest.virtualbox.org>
    Co-authored-by: John Tranier <john.tranier@isae-supaero.fr>

--- lang/en/naas.php
+++ lang/en/naas.php
@@ -16,150 +16,136 @@
 
 /**
  * Strings for component 'url'; language 'en'; branch 'MOODLE_20_STABLE'
- *
+ * English messages.
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @copyright (C) 2019  ISAE-SUPAERO (https://www.isae-supaero.fr/)
  * @package mod_naas
  */
 
-$string['pluginname'] = 'Nugget';
+// Clés générales.
+$string['modulename'] = 'nugget';
 $string['naas:addinstance'] = 'Add a new NaaS module';
-$string['modulename'] = 'Nugget';
-$string['modulename_help'] = 'The Moodle Nugget Plugin enables a teacher to integrate a micro-content from the NaaS Server.
-
-
-The teacher can allow the Nugget exercice to be attempted multiple times. A time limit may be set.
-
-Each attempt is marked automatically and the grade is recorded in the gradebook.
-
-Nuggets may be used
-
-* As course exams
-* As mini tests for reading assignments or at the end of a topic
-* As exam practice using questions from past exams
-* For self-assessment';
+$string['modulename_help'] = '<p>The Moodle Nugget Plugin enables a teacher to integrate a micro-content from the NaaS Server. The teacher can allow the Nugget exercice to be attempted multiple times. A time limit may be set. Each attempt is marked automatically and the grade is recorded in the gradebook.<br>Nuggets may be used:<ul><li>As course exams</li><li>As mini tests for reading assignments or at the end of a topic</li><li>As exam practice using questions from past exams</li><li>For self-assessment</li></ul></p>';
 $string['modulename_link'] = 'mod/naas/view';
 $string['modulenameplural'] = 'Nuggets';
 $string['pluginadministration'] = '';
+$string['pluginname'] = 'nugget';
 $string['nugget'] = 'Nugget';
 
+// Capabilities.
+$string['mod_naas:view'] = 'Can view the Nugget';
+$string['mod_naas:addinstance'] = 'Can add a new Nugget to the course';
+
+// NaaS settings.
+$string['completiondetail:passorexhaust'] = 'Complete on pass or exhaustion';
+$string['completiondetail:passgrade'] = 'Complete on passing grade';
 $string['naas_settings'] = 'NaaS settings';
-$string['naas_settings_information'] = 'The NaaS API documentation may provide information on how to obtain the API endpoint and credentials. You can check their documentation for more information or contact the support team of NaaS.';
-$string['naas_settings_endpoint'] = 'NaaS API endpoint';
-$string['naas_settings_endpoint_help'] = 'Enter the NaaS API endpoint';
-$string['naas_settings_username'] = 'NaaS API user';
-$string['naas_settings_username_help'] = 'Enter the NaaS API user';
-$string['naas_settings_structure_id'] = 'NaaS API structure';
-$string['naas_settings_structure_id_help'] = 'Enter the NaaS API target structure';
-$string['naas_settings_password'] = 'NaaS API password';
-$string['naas_settings_password_help'] = 'Enter the NaaS API password';
-$string['naas_settings_timeout'] = 'NaaS API timeout';
-$string['naas_settings_timeout_help'] = 'Number of seconds to wait before aborting a call to the NaaS API (0 for infinite)';
 $string['naas_settings_css'] = 'NaaS CSS';
 $string['naas_settings_css_help'] = 'Extra CSS to be applied to NaaS ressources)';
-$string['naas_settings_filter'] = 'NaaS search filter';
-$string['naas_settings_filter_help'] = 'A query to filter search results';
+$string['naas_settings_endpoint'] = 'NaaS API endpoint';
+$string['naas_settings_endpoint_help'] = 'Enter the NaaS API endpoint';
 $string['naas_settings_feedback'] = 'NaaS feedback';
 $string['naas_settings_feedback_help'] = 'Enable the option to give learners the ability to provide feedback on Nugget activities.';
-
+$string['naas_settings_filter'] = 'NaaS search filter';
+$string['naas_settings_filter_help'] = 'A query to filter search results';
+$string['naas_settings_information'] = 'The NaaS API documentation may provide information on how to obtain the API endpoint and credentials. You can check their documentation for more information or contact the support team of NaaS.';
+$string['naas_settings_password'] = 'NaaS API password';
+$string['naas_settings_password_help'] = 'Enter the NaaS API password';
 $string['naas_settings_privacy'] = 'NaaS Privacy';
-$string['naas_settings_privacy_information'] = 'The Moodle Nugget Plugin requires the collection and storage of personal data such as a user\'s name and email address in order to improve the user experience by improving anonymous statistical data analysis. The  collected data is stored on the NaaS server and will not be shared with any third parties.';
+$string['naas_settings_privacy_information'] = 'The Moodle Nugget Plugin requires the collection and storage of personal data such as a user\'s name and email address in order to improve the user experience by improving anonymous statistical data analysis. The collected data is stored on the NaaS server and will not be shared with any third parties.';
 $string['naas_settings_privacy_learner_mail'] = 'Collect nugget learners emails';
 $string['naas_settings_privacy_learner_mail_help'] = 'Send the learner email when connecting the user to the NaaS server to help improve the user experience by improving anonymous statistical data analysis';
 $string['naas_settings_privacy_learner_name'] = 'Collect nugget learners names';
 $string['naas_settings_privacy_learner_name_help'] = 'Send the learner name when connecting the user to the NaaS to improve their experience';
+$string['naas_settings_structure_id'] = 'NaaS API structure';
+$string['naas_settings_structure_id_help'] = 'Enter the NaaS API target structure';
+$string['naas_settings_timeout'] = 'NaaS API timeout';
+$string['naas_settings_timeout_help'] = 'Number of seconds to wait before aborting a call to the NaaS API (0 for infinite)';
+$string['naas_settings_username'] = 'NaaS API user';
+$string['naas_settings_username_help'] = 'Enter the NaaS API user';
 
-$string['naas_unable_connect'] = 'Unable to contact the NaaS server';
-
-$string['name_help'] = 'Name of the nugget that will appear in the course section';
-$string['name_display'] = 'Name to display';
+// Moodle Form.
 $string['cgu_agreement'] = 'I have read and agree to the <a target="_blank" href="https://doc.clickup.com/2594656/p/h/2f5v0-13342/fff6a689cd78033">Terms and Conditions of the NaaS platform</a>';
+$string['general'] = 'General';
+$string['lastmodified'] = 'Last modified';
+$string['moduleintro'] = 'Introduction to the module';
+$string['name_display'] = 'Name to display';
+$string['name_help'] = 'Name of the nugget that will appear in the course section';
+$string['naas_unable_connect'] = 'Unable to contact the NaaS server';
+$string['unlimited'] = 'Unlimited';
 
+// Vue Form.
+$string['about'] = 'About';
+$string['back_to_course'] = 'Back to Course Index';
+$string['click_to_replace'] = 'Replace the selected nugget';
+$string['clear_filters'] = 'Clear filters';
+$string['hide_authors'] = 'Hide';
+$string['loading'] = 'Loading...';
+$string['next_unit'] = 'Next Unit';
+$string['no_nugget'] = 'No nugget found';
 $string['nugget_search'] = 'Search Nuggets';
 $string['nugget_search_here'] = 'To get started, enter a keyword';
 $string['nugget_search_no_result'] = 'The search returned no results, please use another keyword.';
-$string['click_to_replace'] = 'Replace the selected nugget';
-$string['no_nugget'] = 'No nugget found';
-$string['clear_filters'] = 'Clear filters';
+$string['preview_button'] = 'Preview';
+$string['select_button'] = 'Select';
 $string['show_more_authors'] = 'Show more';
-$string['hide_authors'] = 'Hide';
-
-$string['about'] = 'About';
-$string['back_to_course'] = 'Back to Course Index';
-$string['next_unit'] = 'Next Unit';
 $string['show_more_nugget_button'] = 'Show more ...';
 
-$string['select_button'] = 'Select';
-$string['preview_button'] = 'Preview';
-$string['loading'] = 'Loading...';
-
-/* Metadata */
-$string["preview"] = "Preview: ";
-$string["description"] = "Description";
-$string["in_brief"] = "In Brief";
-$string["about_author"] = "About the author";
-$string["learning_outcomes"] = "Learning outcomes";
-$string["prerequisites"] = "Prerequisites";
-$string["references"] = "References";
-$string["field_of_study"] = "Field of study";
-$string["authors"] = "Authors";
-$string["producers"] = "Producers";
-$string["language"] = "Language";
-$string["duration"] = "Duration";
-$string["level"] = "Level";
-$string["structure_id"] = "Structure";
-$string["advanced"] = "Advanced";
-$string["intermediate"] = "Intermediate";
-$string["beginner"] = "Beginner";
-$string["tags"] = "Tags";
-$string["type"] = "Type";
-$string["lesson"] = "Lesson";
-$string["tutorial"] = "Tutorial";
-$string["demo"] = "Demo";
-$string["en"] = "English";
-$string["fr"] = "French";
-$string["de"] = "German";
-$string["es"] = "Spanish";
-$string["it"] = "Italian";
-$string["pl"] = "Polish";
-$string["sv"] = "Swedish";
-$string["publication_date"] = "Publication date";
-
-$string['rating_title'] = 'Rate this nugget';
-$string['rating_description'] = 'Your rating will be used to improve the quality of our content';
-$string['rating_send'] = 'Send';
-$string['rating_sent'] = 'Sent ✔';
-
-$string['learning_outcomes_desc'] = 'You have completed this nugget. The learning objectives were:';
-$string['complete_nugget'] = 'I Finished My Learning With This Nugget';
-
-$string['grade_type'] = 'Type of grading';
-$string['attempts_allowed'] = 'Attempts allowed';
-$string['grade_method'] = 'Grading method';
+// Metadata.
+$string['about_author'] = '_About the author_';
+$string['advanced'] = '_Advanced_';
+$string['authors'] = '_Authors_';
+$string['description'] = '_Description_';
+$string['en'] = '_English_';
+$string['field_of_study'] = '_Field of study_';
+$string['fr'] = '_French_';
+$string['de'] = '_German_';
+$string['it'] = '_Italian_';
+$string['es'] = '_Spanish_';
+$string['intermediate'] = '_Intermediate_';
+$string['in_brief'] = '_In Brief_';
+$string['language'] = '_Language_';
+$string['level'] = '_Level_';
+$string['lesson'] = '_Lesson_';
+$string['pl'] = '_Polish_';
+$string['preview'] = '_Preview: _';
+$string['producers'] = '_Producers_';
+$string['publication_date'] = '_Publication date_';
+$string['references'] = '_References_';
+$string['tags'] = '_Tags_';
+$string['tutorial'] = '_Tutorial_';
+$string['structure_id'] = '_Structure_';
+$string['type'] = '_Type_';
+$string['demo'] = '_Demo_';
+$string['sv'] = '_Swedish_';
+$string['beginner'] = '_Beginner_';
 $string['gradehighest'] = 'Highest grade';
 $string['gradeaverage'] = 'Average grade';
-$string['attemptfirst'] = 'First attempt';
-$string['attemptlast'] = 'Last attempt';
-$string['grade_method_help'] = 'When multiple attempts are allowed, the following methods are available for calculating the final nugget grade:
+$string['grade_method'] = 'Grading method';
+$string['grade_method_help'] = '<p>When multiple attempts are allowed, the following methods are available for calculating the final nugget grade:<ul><li>Highest grade of all attempts</li><li>Average (mean) grade of all attempts</li><li>First attempt (all other attempts are ignored)</li><li>Last attempt (all other attempts are ignored)</li></ul></p>';
+$string['grade_type'] = 'Type of grading';
 
-* Highest grade of all attempts
-* Average (mean) grade of all attempts
-* First attempt (all other attempts are ignored)
-* Last attempt (all other attempts are ignored)';
+// Rating.
+$string['complete_nugget'] = 'I Finished My Learning With This Nugget';
+$string['learning_outcomes_desc'] = 'You have completed this nugget. The learning objectives were: ';
+$string['rating_description'] = 'Your rating will be used to improve the quality of our content';
+$string['rating_send'] = 'Send';
+$string['rating_sent'] = 'Sent';
+$string['rating_title'] = 'Rate this nugget';
 
+// Activity Completion section.
 $string['completionpass'] = 'Require passing grade';
 $string['completionpass_desc'] = 'Student must achieve a passing grade to complete this activity';
 $string['completionpass_help'] = 'If enabled, this activity is considered complete when the student receives a pass grade (as specified in the Grade section of the nugget settings) or higher.';
-
-$string['completiondetail:passgrade'] = 'Receive a pass grade';
-
-$string['completionattemptsexhausted'] = 'Or all available attempts completed';
-$string['completionattemptsexhaustedhelp'] = 'Mark nugget complete when the student has exhausted the maximum number of attempts.';
-
-$string['completionminattempts'] = 'Minimum number of attempts:';
+$string['completionminattempts'] = 'Minimum number of attempts: ';
 $string['completionminattemptsdesc'] = 'Minimum number of attempts required: {$a}';
 $string['completionminattemptsgroup'] = 'Require attempts';
 $string['completionminattemptserror'] = 'Minimum number of attempts must be lower or equal to attempts allowed.';
 
+// Activity Completion section error messages.
+$string['gradecategoryonmodform'] = 'Grade category on mod form';
+$string['gradetopass'] = 'Grade to pass';
 $string['gradetopassnotset'] = 'This nugget does not yet have a grade to pass set. It may be set in the Grade section of the nugget settings.';
 $string['gradetopassmustbeset'] = 'Grade to pass cannot be zero as this nugget has its completion method set to require passing grade. Please set a non-zero value.';
+$string['gradingmethod'] = 'Grading method';
+$string['gradingmethods'] = 'Grading methods';
