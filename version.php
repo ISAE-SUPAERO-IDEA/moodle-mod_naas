bb24a1a2b77b005d50dad7e61e32b52124d80205
John Tranier <john.tranier@isae-supaero.fr>
2025-02-10 13:38:17 +0100

    Test 2.2.8

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version        = 2025021001;                  // The current module version (Date: YYYYMMDDXX).
-$plugin->release        = "2.2.7";
+$plugin->version        = 2025021002;                  // The current module version (Date: YYYYMMDDXX).
+$plugin->release        = "2.2.8";
 $plugin->requires       = 2022041900;                  // Requires this Moodle version.
 $plugin->component      = 'mod_naas';                  // Full name of the plugin (used for diagnostics).
 $plugin->cron           = 0;                           // Frequency of the plugin's cron task.
