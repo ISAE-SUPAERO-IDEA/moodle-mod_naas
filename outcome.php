34fce675df1d0e6832cbc219bb0c50bf6a25c38f
John Tranier <john.tranier@isae-supaero.fr>
2025-02-10 13:42:42 +0100

    Test 2.2.9

--- outcome.php
+++ outcome.php
@@ -26,7 +26,7 @@ require_once($_SERVER['DOCUMENT_ROOT']."/config.php");
 require_once($CFG->dirroot.'/mod/lti/locallib.php');
 require_once("$CFG->libdir/completionlib.php");
 require_once('classes/completion/custom_completion.php');
-require_login(null, false);
+//require_login(null, false);
 
 $entitybody = file_get_contents('php://input');
 
