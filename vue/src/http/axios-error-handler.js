f89b43c6d92472f98cdc40c73b9b0a158ec396f2
Thomas DELALBRE <t.delalbre@mac-delalbre.local>
2025-03-19 22:07:39 +0100

    adding copyright comments on top of the js files

--- vue/src/http/axios-error-handler.js
+++ vue/src/http/axios-error-handler.js
@@ -1,3 +1,26 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Axios HTTP error handling for NAAS plugin.
+ *
+ * @module     mod_naas/http/axios-error-handler
+ * @copyright  2023 NAAS Team
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 import axios from "axios";
 import NaasHttpError from "./NaasHttpError";
 
@@ -25,7 +48,7 @@ export default function handleAxiosError(error) {
                 console.warn("🔐 Unauthorized (401) - Check your credentials");
                 break;
             case 403:
-                console.warn("⛔ Forbidden (403) - You don’t have permission.");
+                console.warn("⛔ Forbidden (403) - You don't have permission.");
                 break;
             case 404:
                 console.warn("🔍 Not Found (404) - The resource does not exist.");
