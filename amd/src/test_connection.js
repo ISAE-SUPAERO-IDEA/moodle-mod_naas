e1748d27c1bab503e5045817f93275932182bfd2
Thomas DELALBRE <t.delalbre@mac-delalbre-w.isae.fr>
2025-03-24 13:46:47 +0100

    rebase and push to fix ci/cd

--- amd/src/test_connection.js
+++ amd/src/test_connection.js
@@ -1,4 +1,31 @@
+<<<<<<< HEAD
 define('mod_naas/test_connection', ['jquery', 'core/ajax', 'core/str'], function($, Ajax, Str) {
+=======
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * JavaScript for testing the connection to the NAAS server.
+ *
+ * @module     mod_naas/test_connection
+ * @copyright  2023 NAAS Team
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+define('mod_naas/test_connection', ['jquery', 'core/ajax'], function($) {
+>>>>>>> f89b43c (adding copyright comments on top of the js files)
     return {
         init: function() {
             $('#testconnection').on('click', function(e) {
