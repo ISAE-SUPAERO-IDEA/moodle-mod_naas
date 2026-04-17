4a003f0e7a8841b5b48424fa40e146fafead6f67
Thomas Delalbre <thomas.delalbre@isae-supaero.fr>
2026-04-17 23:04:33 +0200

    feat(amd): add widget_init.js AMD module to initialize Vue widget config

---
+++ amd/src/widget_init.js
@@ -0,0 +1,34 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Initializes the NaaS Vue widget configuration by exposing it globally.
+ *
+ * @module     mod_naas/widget_init
+ * @copyright  2019 ISAE-SUPAERO (https://www.isae-supaero.fr/)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+define([], function() {
+    return {
+        /**
+         * Initialize the Widget with configuration.
+         *
+         * @param {Object} config The widget configuration object passed from PHP's js_call_amd.
+         */
+        init: function(config) {
+            window.NAAS = config;
+        }
+    };
+});
