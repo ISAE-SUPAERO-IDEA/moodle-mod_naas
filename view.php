3b143802fbcb11031b03218c7abf81b87a9d8b7d
Thomas Delalbre <thomas.delalbre@isae-supaero.fr>
2026-04-17 23:13:53 +0200

    refactor: index, view and amd to use mustache templates

--- view.php
+++ view.php
@@ -60,31 +60,16 @@ $PAGE->set_heading($course->fullname);
 echo $OUTPUT->header();
 
 $courseurl = new moodle_url('/course/view.php', ['id' => $COURSE->id]);
-// Back to course button.
-$backcoursebutton = "<div class='course-button'><a class='btn btn-outline-secondary btn-sm'
-    href=".$courseurl.">".get_string('back_to_course', 'naas')."</a></div>";
+$backtocourse = get_string('back_to_course', 'naas');
+$nextactivity = \mod_naas\mod_util::get_next_activity_url();
+$widgethtml = \mod_naas\naas_widget::naas_widget_html($naasinstance->nugget_id, $cm->course, $cm->id, "NuggetView");
 
-echo $backcoursebutton;
+$renderer = $PAGE->get_renderer('mod_naas');
+$viewpage = new \mod_naas\output\view_page($courseurl, $backtocourse, $nextactivity, $widgethtml);
 
-$nextactivityurl = \mod_naas\mod_util::get_next_activity_url();
-if ($nextactivityurl) {
-    echo "<div class='next-activity hidden'><a class='btn btn-outline-secondary btn-sm'
-    href=".$nextactivityurl->link."&forceview=1>".$nextactivityurl->name."</a></div>";
-}
+$PAGE->requires->js_call_amd('mod_naas/view_page', 'init');
 
-// Displays Nugget.
-echo \mod_naas\naas_widget::naas_widget_html($naasinstance->nugget_id, $cm->course, $cm->id, "NuggetView");
-
-// Toggles the Nugget 'About' Modal.
-echo "<script>
-let about_button = document.querySelector('.secondary-navigation nav ul li[data-key=about]');
-if (about_button){
-    let widget = document.querySelector('#nugget-info-button div a');
-    about_button.onclick = function() { widget.click(); };
-}
-</script>";
-
-echo $backcoursebutton;
+echo $renderer->render($viewpage);
 
 // Finish the page.
 echo $OUTPUT->footer();
